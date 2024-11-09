33563db59f6441610534b3c3f37401ca959a40ba
Photon13 <tech7137@gmail.com>
2024-11-09 14:02:23 +0100

    Created local repository.

---
+++ amplitudeModulation_V0_0_0/globals_V0_0_0.py
@@ -0,0 +1,16 @@
+from pathlib import Path
+import os
+
+_Version = "_V0_0_0"
+
+#class GlobalPaths:
+    
+    #def getPaths():
+
+path_cwd = os.get_cwd()
+# e.g. path_cwd = .../amplitudeModulation/amplitudeModulation_programs/amplitudeModulation_V0_0_0
+path_test = os.path.join(path_cwd, f"test{_Version}")
+# e.g. .../amplitudeModulation_V0_0_0/test_V0_0_0
+
+print("Hallo {path_test}")
+
---
+++ amplitudeModulation_V0_0_0/helpMethodsExcelJson_V0_0_0.py
@@ -0,0 +1,9 @@
+import os
+from pathlib import Path
+import pandas as pd
+import json
+import typing
+from typing import List
+import datetime
+
+# import other classes
