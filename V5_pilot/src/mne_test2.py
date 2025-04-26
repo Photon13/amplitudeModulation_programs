d078c946276656351588da56519e4fefbfec1d9b
Photon13 <tech7137@gmail.com>
2025-04-27 00:56:00 +0200

    advanced epochs for mne pilot maik. not done yet

--- V5_pilot/src/mne_test2.py
+++ V5_pilot/src/mne_test2.py
@@ -1,81 +1,110 @@
 import matplotlib.pyplot as plt
 from pathlib import Path
 import mne
+from typing import List
+import numpy as np
 
 from Paths import Paths
 
-pathRaw : Path = Path(Paths.PATH_FOLDER_BRAINVISION_RECORDER / "rohDaten" / f"maik_pilot0.vhdr" )
 
+
+@staticmethod
+def plotAllEvents_inRaw(rawBV, events : np.ndarray, event_id : dict):
+    mne.viz.plot_events(events = events, event_id = event_id, sfreq = rawBV.info["sfreq"])
+    # <marker name> (<nr>) : <nr> is number of occurrences marker
+    # sfreq == sample frequency
+    inp = input("Continue? [any]: ")
+
+
+@staticmethod
+def get_events():
+    allEvents = mne.events_from_annotations(
+        raw = rawBV, 
+        event_id = {
+        "StartRecording" : 99999,
+        "shiftLeft" : 32, 
+        "shiftMiddle" : 2,
+        "shiftRight" : 1,
+        "zBus" : 65,
+        "button" : 128
+        }
+    )[0]
+
+    shiftButtonEvents = mne.events_from_annotations(
+        raw = rawBV, 
+        event_id = {
+        "shiftLeft" : 32, 
+        "shiftMiddle" : 2,
+        "shiftRight" : 1,
+        "button" : 128
+        }
+    )[0]
+    # shiftButtonEvents == [[ 10613      0      1] [ 11613      0      1] ... ]
+
+    zBusEvents = mne.events_from_annotations(
+        raw = rawBV, 
+        event_id = {
+        "zBus" : 65
+        }
+    )[0]
+    return allEvents, shiftButtonEvents, zBusEvents
+
+
+
+
+pathRaw : Path = Path(Paths.PATH_FOLDER_BRAINVISION_RECORDER / "rohDaten" / f"maik_pilot0.vhdr" )
 rawBV = mne.io.read_raw_brainvision(pathRaw, preload = True, verbose = False)
 
+# FILTERING
 rawBV.filter(l_freq = 20, h_freq = 60)
 rawBV.info["line_freq"] = 50.0
 
-annotationDict = ({
+
+# RENAMING ANNOTATIONS: "Stimulus/S 32" into "shiftLeft" etc.
+rawBV.annotations.rename({
+    "New Segment/" : "StartRecording",
     "Stimulus/S 32" : "shiftLeft", 
     "Stimulus/S  2" : "shiftMiddle",
     "Stimulus/S  1" : "shiftRight",
     "Stimulus/S 65" : "zBus",
-    "Stimulus/S 66" : "failedBlockStart",
     "Stimulus/S128" : "button"
 })
+# rawBV.annotations == <Annotations | 322 segments: StartRecording/ (1), shiftLeft (95), ...>
+# number in brackets is respective number of occurrences marker
 
-rawBV.annotations.rename(annotationDict)
-
-eventDict_nameId = ({
-    "shiftLeft" : 32, 
-    "shiftMiddle" : 2,
-    "shiftRight" : 1,
-    "zBus" : 65,
-    "failedBlockStart" : 66,
-    "button" : 128
-})
+allEvents, shiftButtonEvents, zBusEvents = get_events()
 
 
-allEvents, allEvent_id = mne.events_from_annotations(raw = rawBV, event_id = eventDict_nameId)
-# allEvents : NDarray[] == [[     0      0  99999] [  7113      0     65] [ 10613      0      1] ... ]
-# allEvent_id : dict == {'New Segment/': 99999, 'Stimulus/S  1': 1, 'Stimulus/S  2': 2, 'Stimulus/S 32': 32, 'Stimulus/S 65': 65, 'Stimulus/S 66': 66, 'Stimulus/S128': 128}
-print(allEvents)
-print(allEvent_id)
+#plotAllEvents_inRaw(rawBV, allEvents, allEvent_id)
 
-mne.viz.plot_events(events = allEvents, event_id = allEvent_id, sfreq = rawBV.info["sfreq"])
-# nr behind marker name is number of occurrences
-# sfreq == sample frequency
-inp = input("Continue? [any]: ")
 
-rowEvents = [
-    "shiftLeft", 
-    "shiftMiddle",
-    "shiftRight",
-    "zBus",
-    "failedBlockStart",
-    "button"
-]
-
-occurenceTimes_zBus = [] 
-for i in range(len(allEvents)):
-    if(allEvents[i][2] == eventDict_nameId["zBus"]):
-        # allEvents[i][2] last entry of sub-NDarray
-        occurenceTimes_zBus.append(allEvents[i][0])
-        # allEvents[i][0] first entry of sub-NDarray
-print(occurenceTimes_zBus)
-#occurenceTimes_zBus = [7113, 47143, 87170, 130914, 172943, 216291, 259598, 318812, 360714, 405850]
-
-# Continue? [any]: j
-
-blockNr = 0
-
-tmin = occurenceTimes_zBus[blockNr] # change to sec instead of samples?
-tmax = tmin + 40
-rawBV.crop(tmin = tmin, tmax = tmax, include_tmax = True)
-metadata_block0, events_block0, event_id_block0 = mne.epochs.make_metadata(
-    events = allEvents,
-    event_id = allEvent_id,
+metadata_shiftButtonEvents = mne.epochs.make_metadata(
+    events = shiftButtonEvents,
+    event_id = {
+        "shiftLeft" : 32, 
+        "shiftMiddle" : 2,
+        "shiftRight" : 1,
+        "button" : 128
+        },
     tmin = 0.0,
     tmax = 2.0,
     sfreq = rawBV.info["sfreq"],
-    row_events = rowEvents,
-)
-print(metadata_block0)
+    row_events = ["shiftLeft", "shiftMiddle", "shiftRight"]
+)[0]
+print(metadata_shiftButtonEvents)
 
-# undo renaming 65 -> 66 ! (for addressing per block nr)
+
+
+
+
+dictBlockData : dict = {}
+for i in range(0, 19, 2): # n blocks total for pilot maik == 20
+    blockNr = i
+    tmin = timePointsZBus[blockNr] / rawBV.info["sfreq"] # sec
+    tmax = tmin + 40 # sec
+    rawBV_block = rawBV.crop(tmin = tmin, tmax = tmax, include_tmax = True)
+    
+
+    blockData = [metadata, events_block, event_id_block]
+    dictBlockData[f"block{blockNr}"] = blockData
+print(dictBlockData)
