e7369f2fa4780c8dda423576439e8463557b5deb
Photon13 <tech7137@gmail.com>
2025-04-27 12:56:57 +0200

    improved functionality by creating cropped version of raw covering a single block. However, not working because raw.copy().crop() throws Exception.

--- V5_pilot/src/mne_test2.py
+++ V5_pilot/src/mne_test2.py
@@ -3,9 +3,18 @@ from pathlib import Path
 import mne
 from typing import List
 import numpy as np
+import sys
 
 from Paths import Paths
 
+COLORBLUE   = '\33[34m'
+COLORGREEN = "\033[0;32m"
+COLORRED    = '\33[31m'
+COLORCYAN = '\033[36m'
+COLORPURPLE = '\033[35m'
+COLORYELLOW = '\033[33m'
+COLORFAT = '\033[1m'
+COLOREND = '\033[0m'
 
 
 @staticmethod
@@ -15,12 +24,11 @@ def plotAllEvents_inRaw(rawBV, events : np.ndarray, event_id : dict):
     # sfreq == sample frequency
     inp = input("Continue? [any]: ")
 
-
 @staticmethod
-def get_events():
-    allEvents = mne.events_from_annotations(
-        raw = rawBV, 
-        event_id = {
+def get_eventIdDict(eventType):
+    """ eventTypes == "all" | "zBus" | "buttonsAndShifts" """
+    if( eventType == "all"):
+        return {
         "StartRecording" : 99999,
         "shiftLeft" : 32, 
         "shiftMiddle" : 2,
@@ -28,27 +36,20 @@ def get_events():
         "zBus" : 65,
         "button" : 128
         }
-    )[0]
-
-    shiftButtonEvents = mne.events_from_annotations(
-        raw = rawBV, 
-        event_id = {
+    elif( eventType == "zBus"):
+        return {
+        "zBus" : 65
+        }
+    elif( eventType == "buttonsAndShifts"):
+        return {
         "shiftLeft" : 32, 
         "shiftMiddle" : 2,
         "shiftRight" : 1,
         "button" : 128
         }
-    )[0]
-    # shiftButtonEvents == [[ 10613      0      1] [ 11613      0      1] ... ]
-
-    zBusEvents = mne.events_from_annotations(
-        raw = rawBV, 
-        event_id = {
-        "zBus" : 65
-        }
-    )[0]
-    return allEvents, shiftButtonEvents, zBusEvents
-
+    else:
+        print(COLORRED + "Invalid eventType!" + COLOREND)
+        sys.exit()
 
 
 
@@ -72,31 +73,49 @@ rawBV.annotations.rename({
 # rawBV.annotations == <Annotations | 322 segments: StartRecording/ (1), shiftLeft (95), ...>
 # number in brackets is respective number of occurrences marker
 
-allEvents, shiftButtonEvents, zBusEvents = get_events()
 
 
-#plotAllEvents_inRaw(rawBV, allEvents, allEvent_id)
+allEvents, allEvent_id = mne.events_from_annotations(
+    raw = rawBV, 
+    event_id = get_eventIdDict("all")
+)
+# plotAllEvents_inRaw(rawBV, allEvents, allEvent_id)
 
 
-metadata_shiftButtonEvents = mne.epochs.make_metadata(
-    events = shiftButtonEvents,
-    event_id = {
-        "shiftLeft" : 32, 
-        "shiftMiddle" : 2,
-        "shiftRight" : 1,
-        "button" : 128
-        },
+
+zBusEvents = mne.events_from_annotations(
+    raw = rawBV, 
+    event_id = get_eventIdDict("zBus")
+)
+
+nrCurrentBlock = 0
+start_blockX = zBusEvents[nrCurrentBlock][2] /rawBV.info["sfreq"]
+end_blockX = start_blockX + 40
+rawBV_blockX = rawBV.copy().crop(tmin = start_blockX, tmax = end_blockX, include_tmax = False)
+# (drop last sec)
+###### The truth value of an array with more than one element is ambiguous. Use a.any() or a.all()
+buttonsAndShiftsEvents_blockX = mne.events_from_annotations(
+    raw = rawBV_blockX, 
+    event_id = get_eventIdDict("buttonsAndShiftsEvents")
+)
+
+
+epochs_bockX = mne.Epochs(rawBV_blockX, buttonsAndShiftsEvents_blockX)
+
+metadata_buttonsAndShiftsEvents_blockX = epochs_bockX.make_metadata(
+    events = buttonsAndShiftsEvents_blockX,
+    event_id = get_eventIdDict("buttonsAndShiftsEvents"),
     tmin = 0.0,
     tmax = 2.0,
     sfreq = rawBV.info["sfreq"],
     row_events = ["shiftLeft", "shiftMiddle", "shiftRight"]
 )[0]
-print(metadata_shiftButtonEvents)
+print(metadata_buttonsAndShiftsEvents_blockX)
 
 
 
 
-
+###### ?
 dictBlockData : dict = {}
 for i in range(0, 19, 2): # n blocks total for pilot maik == 20
     blockNr = i
